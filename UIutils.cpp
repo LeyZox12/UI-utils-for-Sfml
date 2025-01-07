0b255ce1d065d7e3a7c0952d86c93b7a02a52f76
LeyZox12 <161898497+LeyZox12@users.noreply.github.com>
2025-01-07 16:05:37 +0100

    Add files via upload

--- UIutils.cpp
+++ UIutils.cpp
@@ -14,32 +14,15 @@ UIutils::UIutils()
 {
 
 }
-static bool UIutils::rectVecCollision(Vector2i vec, RectangleShape rect)
-{
-    return (rect.getPosition().x < vec.x &&
-            rect.getPosition().x + rect.getSize().x > vec.x &&
-            rect.getPosition().y < vec.y &&
-            rect.getPosition().y + rect.getSize().y > vec.y);
-}
-static string toString(int val)
-{
-    stringstream stream;
-    stream<<val;
-    string strbuffer;
-    stream>>strbuffer;
-    return strbuffer;
-}
-void UIutils::setWindow(RenderWindow &window)
-{
-    this->window = window;
-}
 void UIutils::addButton(Vector2f pos, Vector2f sizeRect, function<void()> command, string name)
 {
     button newButton;
     newButton.pos = pos;
     newButton.buttonSize = sizeRect;
+    newButton.buttonRect.setPosition(pos);
+    newButton.buttonRect.setSize(sizeRect);
     newButton.command = command;
-    newButton.buttonName = name;
+    newButton.buttonNameStr = name;
     newButton.font = font;
     buttons.push_back(newButton);
 }
@@ -48,6 +31,8 @@ void UIutils::addDropDown(Vector2f pos, Vector2f sizeRect, vector<string> elemen
     dropDown newDropDown;
     newDropDown.pos = pos;
     newDropDown.sizeRect = sizeRect;
+    newDropDown.dropRect.setPosition(pos);
+    newDropDown.dropRect.setSize(sizeRect);
     newDropDown.font = font;
     for(auto& s : elements)
         newDropDown.addElement(s);
@@ -59,6 +44,8 @@ void UIutils::addSlider(Vector2f pos, Vector2f sizeRect, float maxVal, bool hasK
     slider newSlider;
     newSlider.pos = pos;
     newSlider.sizeRect = sizeRect;
+    newSlider.backgroundRect.setPosition(pos);
+    newSlider.backgroundRect.setSize(sizeRect);
     newSlider.maxValue = maxVal;
     newSlider.hasknob = hasKnob;
     newSlider.font = font;
@@ -69,22 +56,25 @@ void UIutils::addTextZone(Vector2f pos, Vector2f sizeRect, int txtSize)
     textZone newTextZone;
     newTextZone.pos = pos;
     newTextZone.sizeRect = sizeRect;
+    newTextZone.textZoneRect.setPosition(pos);
+    newTextZone.textZoneRect.setSize(sizeRect);
     newTextZone.txtSize = txtSize;
     newTextZone.font = font;
 }
-void UIutils::updateElements(Event e)
+void UIutils::updateElements(Event e, RenderWindow &window)
 {
-    mousepos = Mouse::getPosition(window);
-    if(e.type == MouseButtonPressed && e.mouseButton.button == Mouse::Left)
+
+    if(e.type == Event::MouseButtonPressed && e.mouseButton.button == Mouse::Left)
     {
-        for(button b : buttons)
-            b.onClick(mousepos);
-        for(dropDown : dropDowns)
-            d.getSelected(mousepos, true);
-        for(textZone t : textZones)
-            t.updateShouldWrite(mousepos);
-        for(slider s : sliders)
-            s.updateValue(mousepos);
+        Vector2i mousepos = Mouse::getPosition(window);
+        for(int i = 0; i < buttons.size(); i++)
+            buttons[i].onClick(mousepos);
+        for(int i = 0; i < dropDowns.size(); i++)
+            cout << dropDowns[i].getSelected(mousepos, true) << endl;
+        for(int i = 0; i < textZones.size(); i++)
+            textZones[i].updateShouldWrite(mousepos);
+        for(int i = 0; i < sliders.size(); i++)
+            sliders[i].updateValue(mousepos);
     }
     else if(e.type == Event::TextEntered)
     {
@@ -96,11 +86,12 @@ void UIutils::updateElements(Event e)
     }
     else if(e.type == Event::MouseWheelMoved)
     {
-        for(dropDown d : dropDowns)
-            d.scroll(e.mouseWheel.delta);
+        Vector2i mousepos = Mouse::getPosition(window);
+        for(int i = 0; i < dropDowns.size(); i++)
+            dropDowns[i].scroll(e.mouseWheel.delta, mousepos);
     }
 }
-void UIutils::displayElements()
+void UIutils::displayElements(RenderWindow &window)
 {
     for(button b : buttons)
         b.display(window);
--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ Vector2i mousepos;
 RenderWindow window(VideoMode(512,512), "UILib");
 UIutils ui;
 Event e;
+void test();
 /*
 0=>Button
 1=>Slider
@@ -29,11 +30,14 @@ void test()
 }
 int main()
 {
-    ui.setWindow(window);
     if(!font.loadFromFile("res/font.ttf"))
         cout << "error, couldnt load font.ttf";
+    ui.font = font;
+    ui.addButton(Vector2f(0,0),Vector2f(200,50), test, "testButton");
+    ui.addSlider(Vector2f(200,0), Vector2f(200,50), 200, false);
+    vector<string> testValues = {"1","4","5","4","3"};
 
-
+    ui.addTextZone(Vector2f(600,0), Vector2f(200,50), 20);
     while(window.isOpen())
     {
         while(window.pollEvent(e))
@@ -45,10 +49,11 @@ int main()
                 FloatRect visibleArea(0, 0, e.size.width, e.size.height);
                 window.setView(View(visibleArea));
             }
+            ui.updateElements(e, window);
 
         }
         window.clear(Color::Black);
-
+        ui.displayElements(window);
         window.display();
     }
     return 0;
